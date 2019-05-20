f7f0befd9d520447afddef61297d8e00821d37f9
Laura Demessine <laura.demessine@gmail.com>
2019-05-20 21:11:56 +0200

    :fearue: add animation on landing page

--- js/main.js
+++ js/main.js
@@ -1,50 +1,33 @@
-// Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {  
- 
-  let body = req.body;
+document.addEventListener('DOMContentLoaded', () => {
 
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
+    console.log("dom loaded")
 
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
+    let controller = new ScrollMagic.Controller();
 
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-    });
+    let timeline = new TimelineMax();
+    timeline
+    .to('#shape', 6, {
+        y: -30
+    })
+    .to('#plant', 6, {
+        y: -50
+    }, '-=6')
+    .to('#girl', 6, {
+        y: -70
+    }, '-=6')
+    .from('.formation', 3, {
+        top: '40px',
+        autoAlpha: 0
+    }, '-=4')
+    
 
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
-});
-
-
-
-//PARALLAX//
-
-(function() {
-  window.addEventListener('scroll', function(event) {
-    var depth, i, layer, layers, len, movement, topDistance, translate3d;
-    topDistance = this.pageYOffset;
-    layers = document.querySelectorAll("[data-type='parallax']");
-    for (i = 0, len = layers.length; i < len; i++) {
-      layer = layers[i];
-      depth = layer.getAttribute('data-depth');
-      movement = -(topDistance * depth);
-      translate3d = 'translate3d(0, ' + movement + 'px, 0)';
-      layer.style['-webkit-transform'] = translate3d;
-      layer.style['-moz-transform'] = translate3d;
-      layer.style['-ms-transform'] = translate3d;
-      layer.style['-o-transform'] = translate3d;
-      layer.style.transform = translate3d;
-    }
-  });
-
-}).call(this);
-
+    let scene = new ScrollMagic.Scene({
+        triggerElement: 'home-illustrations',
+        duration: '100%',
+        triggerHook: 0.5
+    })
+    .setTween(timeline)
+    .addIndicators()
+    .setPin('section')
+    .addTo(controller);
+})
